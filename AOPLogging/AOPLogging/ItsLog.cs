583d356aea6ef6383b2c7b3d1fae4d7cda3f07bb
hnorth <harlannorth@gmail.com>
2015-03-18 08:15:45 -0700

    Added Autofac AOP for report logging interception

---
+++ AOPLogging/AOPLogging/ItsLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Castle.DynamicProxy;
+
+
+namespace AOPLogging
+{
+    class ItsLog:IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+
+            var name = string.Format("{0}.{1}", invocation.TargetType.FullName, invocation.MethodInvocationTarget.Name);
+
+            Console.WriteLine("{0}.start", name);
+
+            var timed = new System.Diagnostics.Stopwatch();
+            timed.Start();
+            
+            invocation.Proceed();
+            
+            timed.Stop();
+
+
+            Console.WriteLine("{0}.end", name);
+            Console.Write("{0}:{1}", name, timed.ElapsedMilliseconds);
+            
+        }
+    }
+}
---
+++ AOPLogging/AOPLogging/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extras.DynamicProxy2;
+using Castle.DynamicProxy;
+
+namespace AOPLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new Autofac.ContainerBuilder();
+
+            builder.RegisterType<FancyReport>()
+                .As<FancyReport>()
+                .EnableClassInterceptors()
+                .InterceptedBy(typeof (ItsLog));
+
+            builder.RegisterType<LoggedType2>()
+                .As<LoggedType2>()
+                .EnableClassInterceptors()
+                .InterceptedBy(typeof(ItsLog));
+
+
+            builder.RegisterType<ItsLog>();
+            //typed registration
+            builder.RegisterCallback(c => new ItsLog());
+
+            var container = builder.Build();
+
+            //use it
+            var intercepted = container.Resolve<FancyReport>();
+            intercepted.DoReport("first");
+            intercepted.DoReport("second");
+
+            var intercepted2 = container.Resolve<LoggedType2>();
+            intercepted2.DoReport("first");
+            intercepted2.DoReport("second");
+
+        }
+
+     
+    }
+
+    public interface IReport
+    {
+        void DoReport(string something);
+    }
+
+    public class FancyReport : IReport
+    {
+        public virtual void DoReport(string something)
+        {
+            Console.WriteLine("Report {0}", something);
+        }
+
+    }
+
+    public class LoggedType2 : IReport
+    {
+        public virtual void DoReport(string something)
+        {
+            CalledSomething(something);
+        }
+
+        public void CalledSomething(string something)
+        {
+            Console.WriteLine("Do {0}", something);
+        }
+
+    }
+    
+}
